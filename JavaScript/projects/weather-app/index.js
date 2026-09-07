cdfd04f03853673d677f8bf275413fd98a575804
Aashwin Shukla <aashwinshukla585@gmail.com>
2026-09-07 19:58:18 +0530

    building js for weather app

--- JavaScript/projects/weather-app/index.js
+++ JavaScript/projects/weather-app/index.js
@@ -1,11 +1,35 @@
-const myBtn = document.getElementById("myBtn").value;
-const name = document.getElementById("name").value;
-const temp = document.getElementById("temp").value;
-const humidity = document.getElementById("humidity").value;
-const sky = document.getElementById("sky").value;
-const emoji = document.getElementById("emoji").value;
-const city = "";
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const display = document.querySelector(".display");
+const apiKey = "2abbd2c83aaa41bb1ab482abe3d279e0";
 
-submit(){
-    city = ${}
+weatherForm.addEventListener("submit", event =>{
+    
+    event.preventDefault();
+
+    const city = cityInput.value;
+    
+    if(city){
+
+    }else{
+        displayError();
+        
+    }
+});
+
+
+async function getWeatherData(city){
+
 }
+
+function getWeatherInfo(data){
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function displayError(message){
+
+} 
