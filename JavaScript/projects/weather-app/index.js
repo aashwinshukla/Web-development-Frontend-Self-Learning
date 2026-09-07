94482d9818b935aed495e80a0715f92c54dfe1f9
Aashwin Shukla <aashwinshukla585@gmail.com>
2026-09-07 20:10:39 +0530

    validation check to get city

--- JavaScript/projects/weather-app/index.js
+++ JavaScript/projects/weather-app/index.js
@@ -1,6 +1,6 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
-const display = document.querySelector(".display");
+const card = document.querySelector(".card");
 const apiKey = "2abbd2c83aaa41bb1ab482abe3d279e0";
 
 weatherForm.addEventListener("submit", event =>{
@@ -12,8 +12,8 @@ weatherForm.addEventListener("submit", event =>{
     if(city){
 
     }else{
-        displayError();
-        
+        displayError("Please Enter a city");
+
     }
 });
 
@@ -32,4 +32,11 @@ function getWeatherEmoji(weatherId){
 
 function displayError(message){
 
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errprDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
 } 
