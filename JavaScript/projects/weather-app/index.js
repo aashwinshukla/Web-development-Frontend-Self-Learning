ae918d3736847f84a5674b702208561391a72ba7
Aashwin Shukla <aashwinshukla585@gmail.com>
2026-09-07 14:21:47 +0530

    styling the weather projrct

---
+++ JavaScript/projects/weather-app/index.js
@@ -0,0 +1,11 @@
+const myBtn = document.getElementById("myBtn").value;
+const name = document.getElementById("name").value;
+const temp = document.getElementById("temp").value;
+const humidity = document.getElementById("humidity").value;
+const sky = document.getElementById("sky").value;
+const emoji = document.getElementById("emoji").value;
+const city = "";
+
+submit(){
+    city = ${}
+}
