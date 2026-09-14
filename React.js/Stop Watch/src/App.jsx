8ba66a59de8fcecc456482c99f9fffb22b616af2
Aashwin Shukla <aashwinshukla585@gmail.com>
2026-09-14 13:54:34 +0530

    multiple stop watch

--- React.js/Stop Watch/src/App.jsx
+++ React.js/Stop Watch/src/App.jsx
@@ -4,9 +4,14 @@ import StopWatch from "./StopWatch"
 function App() {
   
   return (
-    
-      
+    <>
     <StopWatch/>
+    <StopWatch/>
+    <StopWatch/>
+    
+    </>
+      
+    
   )
 }
 
