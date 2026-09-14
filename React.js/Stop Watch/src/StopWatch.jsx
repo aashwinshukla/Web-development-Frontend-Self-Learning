a5d9197e43d3b67e7235790e11b125c864d6651e
Aashwin Shukla <aashwinshukla585@gmail.com>
2026-09-14 13:54:17 +0530

    final formating and display

--- React.js/Stop Watch/src/StopWatch.jsx
+++ React.js/Stop Watch/src/StopWatch.jsx
@@ -38,11 +38,21 @@ function StopWatch(){
 
     function formateTime(){
 
-        
+        let hours = Math.floor(elapseTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapseTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapseTime / (1000) % 60 );
+        let milliseconds = Math.floor((elapseTime % 1000) / 10);
 
-        return `00:00:00`;
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+
+        return `${hours}:${minutes}:${seconds}:${milliseconds}`;
     }
 
+
+    
 
     return( <div className="stopwatch">
                 <div className="display">{formateTime()}</div>
